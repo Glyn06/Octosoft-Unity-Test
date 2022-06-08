5be6cd378fc77ce092b9497aa19b3ab893f23a0c
José Quintana Martín <jquintanamartin@hotmail.com>
2022-06-07 23:47:23 -0300

    Clickable objects

---
+++ Octosoft Unity technical test/Assets/Scripts/ClickableObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableObject : MonoBehaviour
+{
+    public float despawnTime = 5f;
+    private float timer;
+
+    public int reduceScoreBy = 1;
+    public int increaseScoreBy = 5;
+
+    private void Awake()
+    {
+        RaycastClick raycastClick = FindObjectOfType<RaycastClick>();
+        raycastClick.onHitDelegate += OnClick;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= despawnTime)
+        {
+            //Subtract points
+            Destroy(gameObject); //Pool system
+        }
+    }
+
+    public virtual void OnClick()
+    {
+        //add points
+        Destroy(gameObject); //Pool system
+    }
+}
---
+++ Octosoft Unity technical test/Assets/Scripts/RaycastClick.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Camera))]
+public class RaycastClick : MonoBehaviour
+{
+    public float distance = 1000f;
+    public LayerMask layerTarget;
+
+    public delegate void OnHitDelegate();
+    public OnHitDelegate onHitDelegate;
+
+    private Camera cam;
+    private Ray ray;
+    private RaycastHit hit;
+
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = new Ray(
+                cam.ScreenToWorldPoint(Input.mousePosition),
+                cam.transform.forward);
+
+            if (Physics.Raycast(ray, out hit, distance, layerTarget))
+            {
+                onHitDelegate.Invoke();
+            }
+        }
+    }
+}
